9618333e1c77c310f97974e93c5f505a93128643
statusvisualeffect <154653760+statusvisualeffect@users.noreply.github.com>
2024-06-28 21:47:12 -0400

    Update loadScreenshots.js

--- loadScreenshots.js
+++ loadScreenshots.js
@@ -2,17 +2,25 @@ document.addEventListener("DOMContentLoaded", function () {
 	const screenshotsFolder = "screenshots/"
 	const screenshotsContainer = document.getElementById("screenshots")
 	const screenshotFiles = [
-		"1.png",
-		"2.png",
-		"3.png",
-		"4.png"
-	] 
+		{ file: "1.png", title: "ANONIM TOOL - Only Windows OS support" },
+		{ file: "2.png", title: "ANONIM TRMX - Only Termux support" },
+	]
 
-	screenshotFiles.forEach(function (file) {
+	screenshotFiles.forEach(function (item) {
+		const container = document.createElement("div")
+		container.classList.add("screenshot-container")
+
+		const title = document.createElement("div")
+		title.classList.add("screenshot-title")
+		title.textContent = item.title
+
 		const img = document.createElement("img")
-		img.src = screenshotsFolder + file
-		img.alt = file
+		img.src = screenshotsFolder + item.file
+		img.alt = item.title
 		img.classList.add("screenshot")
-		screenshotsContainer.appendChild(img)
+
+		container.appendChild(title)
+		container.appendChild(img)
+		screenshotsContainer.appendChild(container)
 	})
 })
